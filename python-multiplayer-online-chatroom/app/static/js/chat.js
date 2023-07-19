579a6c4fb41dbf3a58b50022c7c7c450b6d96444
Fennec <yuchenguo0309@gmail.com>
2023-07-18 20:36:00 -0500

    half way

--- python-multiplayer-online-chatroom/app/static/js/chat.js
+++ python-multiplayer-online-chatroom/app/static/js/chat.js
@@ -5,6 +5,52 @@ $(document).ready(function () {
     // Get client's name and avatar
     var name = $("#input_name").val();
     var avatar = $("#input_avatar").val();
+    console.log("name below")
+    console.log(name)
+    console.log("avatar below")
+    console.log(avatar)
+
+    // Append chat box
+    function append_msg(name, data) {
+        var html = "";
+        // If me
+        if (data.code == 2) {
+            if (name == data.name) {
+                // data represents localhost
+                html += "<div class=\"row\">";
+                html += "<div class=\"col-md-3\"></div>";
+                html += "<div class=\"col-md-9\">";
+                html += "<div class=\"media\">";
+                html += "<div class=\"media-body\">";
+                html += "<h6 class=\"user-nickname text-right\">" + data.name + "[" + data.dt + "]</h6>";
+                html += "<div class=\"alert alert-success\" role=\"alert\">";
+                html += data.content;
+                html += "</div>";
+                html += "</div>";
+                html += "<img class=\"ml-3 rounded-circle\" style='width: 60px;height: 60px;' src=\"" + data.face + "\">";
+                html += "</div></div></div>";
+            } else {
+                html += "<div class=\"row\">";
+                html += "<div class=\"col-md-9\">";
+                html += "<div class=\"media\">";
+                html += "<img class=\"mr-3 rounded-circle\" style='width: 60px;height: 60px;' src=\"" + data.face + "\">";
+                html += "<div class=\"media-body\">";
+                html += "<h6 class=\"user-nickname\">" + data.name + "[" + data.dt + "]</h6>";
+                html += "<div class=\"alert alert-info\" role=\"alert\">";
+                html += data.content;
+                html += "</div>";
+                html += "</div>";
+                html += "</div></div>";
+                html += "<div class=\"col-md-3\"></div>";
+                html += "</div>";
+            }
+        } else if (data.code == 1) {
+            html += "<p class='text-center text-success'>欢迎" + data.name + "进入聊天室！<img src='/static/images/rorse.gif'><img src='/static/images/rorse.gif'><img src='/static/images/rorse.gif'></p>"
+        }
+        $("#chat-list").append(html);
+        SyntaxHighlighter.highlight();
+        $("#chat-list").scrollTop($("#chat-list").scrollTop() + 9999999);
+    }
 
     // Define connection
     function connect() {
@@ -57,18 +103,17 @@ $(document).ready(function () {
         return obj
     }
 
-    $("send_msg").click( function () {
+
+    $("#send_msg").click(function () {
+        console.log("send_msg button is clicked")
         var msg_data = getFormData();
-        console.log("msg_data below")
-        console.log(msg_data)
-        console.log("msg_data.content below")
-        console.log(msg_data.content)
         if (msg_data.content) {
             msg_data.code = 2;
             ue.setContent('');
+            console.log(msg_data);
             conn.send(JSON.stringify(msg_data));
         } else {
-            alert("Can't send out an empty message.");
+            alert("Can't send an empty message!");
         }
     });
 
